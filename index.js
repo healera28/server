20f7f5f3ba2965aaf3af0a43309c96dc7089372b
healera28 <admin@healera.ru>
2023-07-16 14:30:35 +0600

    Updated accessible routes

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express()
 const PORT = process.env.PORT || 8000
 
 app.use(cors({
-  origin: ["http://localhost:3000", "https://healera.payform.ru", "https://app.super.so", "https://book.healera.ru"],
+  origin: ["https://healera.payform.ru", "https://app.super.so", "https://book.healera.ru", "https://admin.healera.ru"],
   credentials: true
 }))
 
